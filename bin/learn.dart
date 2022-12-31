25f9c30d46ca1b75714a3c2ee6cf1982068e33ee
Gersomsim <gersomhernandez@me.com>
2022-12-30 22:32:47 -0600

    :technologist: Tipos de datos basicos
    Se agregan los tipos de datos basicos para el uso en el lenguaje dart

---
+++ bin/learn.dart
@@ -0,0 +1,23 @@
+import 'package:learn/learn.dart' as learn;
+
+void main(List<String> arguments) {
+  print('Tipos de datos basicos');
+  
+  bool is_boolean = true;
+  int items = 34;
+  double size_box = 23.5;
+  String label = 'Estos son los tipos de datos básicos';
+
+  print( 'Boolean' );
+  print( is_boolean );
+
+  print( 'integer' );
+  print( items );
+
+  print( 'double' );
+  print( size_box );
+
+  print( 'String' );
+  print( label );
+
+}
