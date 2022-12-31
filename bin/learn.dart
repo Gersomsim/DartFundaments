e0ba6e02dab41759fb56ad5c2545f171aabaf1dd
Gersomsim <gersomhernandez@me.com>
2022-12-31 15:15:57 -0600

    :technologist: Tipos de datos avanzados

--- bin/learn.dart
+++ bin/learn.dart
@@ -1,23 +1,47 @@
-import 'package:learn/learn.dart' as learn;
 
 void main(List<String> arguments) {
   print('Tipos de datos basicos');
   
-  bool is_boolean = true;
+  bool isBoolean = true;
   int items = 34;
-  double size_box = 23.5;
+  double sizeBox = 23.5;
   String label = 'Estos son los tipos de datos básicos';
 
   print( 'Boolean' );
-  print( is_boolean );
+  print( isBoolean );
 
   print( 'integer' );
   print( items );
 
   print( 'double' );
-  print( size_box );
+  print( sizeBox );
 
   print( 'String' );
   print( label );
 
+  print('');
+  print('Tipos de datos avanzados');
+
+  var variable = 'El tipo de la variable es auto ingerida y solo puede cambiar de valor';
+  const constante = 3.141519;
+  final gravedad = 9.8;
+
+  dynamic dinamico = 'esta variable puede cambiar de valor y tipo'; 
+
+  print( 'var' );
+  print( variable );
+
+  print( 'const' );
+  print( constante );
+
+  print( 'final' );
+  print( gravedad );
+
+  print( 'dynamic' );
+  print( dinamico );
+
+  dinamico = 4;
+
+  print( 'dynamic' );
+  print( dinamico );
 }
